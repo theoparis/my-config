404ce8a85247a7019853950092c563c9c5315d95
Theo Paris <theoparisdesigns@gmail.com>
2021-03-17 15:27:27 -0700

    Updated stuff

---
+++ scripts/init.zsh
@@ -0,0 +1,18 @@
+if [ -z "${MY_CFG}" ]; then
+    export MY_CFG="$HOME/my-config"
+fi
+
+echo "Attempting to initialize config from ${MY_CFG}."
+
+mkdir -p ~/.config/nvim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+	       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+ln -s "$MY_CFG/dotfiles/vimrc" ~/.config/nvim/init.vim
+ln -s "$MY_CFG/dotfiles/zsh/zshrc" ~/.zshrc
+ln -s "$MY_CFG/dotfiles/tmux.conf" ~/.tmux.conf
+ln -s "$MY_CFG/dotfiles/hyper.js" ~/.hyper.js
+ln -s "$MY_CFG/dotfiles/coc-settings.json" ~/.config/nvim/coc-settings.json
+ln -s "$MY_CFG/dotfiles/starship.toml" ~/.config/starship.toml
+
+echo "Done."
