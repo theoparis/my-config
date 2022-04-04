4e871224591287535d5821b4b4f0a005321f2d01
Theo Paris <theoparisdesigns@gmail.com>
2022-04-04 01:00:53 -0700

    feat: nvim cmp

--- dotfiles/.config/nvim/lua/cmp-config.lua
+++ dotfiles/.config/nvim/lua/cmp-config.lua
@@ -1,65 +1,29 @@
 local cmp = require "cmp"
 
-cmp.setup.cmdline(
-    ":",
-    {
-        sources = cmp.config.sources(
-            {
-                {name = "path"}
-            },
-            {
-                {name = "cmdline"}
-            }
-        )
+cmp.setup({
+    -- Enable LSP snippets
+    snippet = {
+        expand = function(args) require('luasnip').lsp_expand(args.body) end
+    },
+    mapping = {
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        -- Add tab support
+        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+        ["<Tab>"] = cmp.mapping.select_next_item(),
+        ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.close(),
+        ["<CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Insert,
+            select = true
+        })
+    },
+    -- installed sources
+    sources = {
+        {name = "nvim_lsp"}, {name = "path"}, {name = "buffer"},
+        {name = "luasnip"}
     }
-)
+})
 
-cmp.setup.cmdline(
-    "/",
-    {
-        sources = {
-            {name = "buffer"}
-        }
-    }
-)
-
-cmp.setup(
-    {
-        snippet = {
-            -- REQUIRED - you must specify a snippet engine
-            expand = function(args)
-                --vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-                require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-                -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-                -- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
-            end
-        },
-        mapping = {
-            ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), {"i", "c"}),
-            ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), {"i", "c"}),
-            ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), {"i", "c"}),
-            ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-            ["<C-e>"] = cmp.mapping(
-                {
-                    i = cmp.mapping.abort(),
-                    c = cmp.mapping.close()
-                }
-            ),
-            -- Accept currently selected item. If none selected, `select` first item.
-            -- Set `select` to `false` to only confirm explicitly selected items.
-            ["<CR>"] = cmp.mapping.confirm({select = true})
-        },
-        sources = cmp.config.sources(
-            {
-                {name = "nvim_lsp"},
-                --{name = "vsnip"} -- For vsnip users.
-                {name = "luasnip"} -- For luasnip users.
-                -- { name = 'ultisnips' }, -- For ultisnips users.
-                -- { name = 'snippy' }, -- For snippy users.
-            },
-            {
-                {name = "buffer"}
-            }
-        )
-    }
-)
