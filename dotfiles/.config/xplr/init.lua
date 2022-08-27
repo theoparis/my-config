44fd1ad7fe3eab0a117e8c55985dc5b6d5887d49
Theo Paris <theoparisdesigns@gmail.com>
2022-08-26 23:33:22 -0700

    fix: add version to xplr config

--- dotfiles/.config/xplr/init.lua
+++ dotfiles/.config/xplr/init.lua
@@ -1,29 +1,24 @@
-local home = os.getenv 'HOME'
-local xpm_path = home .. '/.local/share/xplr/dtomvan/xpm.xplr'
-local xpm_url = 'https://github.com/dtomvan/xpm.xplr'
+version = "0.19.0"
 
-package.path = package.path
-	.. ';'
-	.. xpm_path
-	.. '/?.lua;'
-	.. xpm_path
-	.. '/?/init.lua'
+local home = os.getenv("HOME")
+local xpm_path = home .. "/.local/share/xplr/dtomvan/xpm.xplr"
 
-require('xpm').setup {
+package.path = package.path .. ";" .. xpm_path .. "/?.lua;" .. xpm_path .. "/?/init.lua"
+
+require("xpm").setup({
 	plugins = {
 		-- Let xpm manage itself
-		'dtomvan/xpm.xplr',
+		"dtomvan/xpm.xplr",
 		{
-			'dtomvan/extra-icons.xplr',
+			"dtomvan/extra-icons.xplr",
 			after = function()
-				xplr.config.general.table.row.cols[2] =
-					{ format = 'custom.icons_dtomvan_col_1' }
+				xplr.config.general.table.row.cols[2] = { format = "custom.icons_dtomvan_col_1" }
 			end,
 		},
-		'prncss-xyz/icons.xplr',
+		"prncss-xyz/icons.xplr",
 	},
 	auto_install = true,
 	auto_cleanup = true,
-}
+})
 
-require('icons').setup()
+require("icons").setup()
