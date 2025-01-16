388605ca54b20b99781bb99f273f034d06674b7d
Theo Paris <theo@tinted.dev>
2025-01-15 22:06:41 -0800

    feat(nvim): add nixfmt

--- dotfiles/.config/nvim/lua/plugins/main.lua
+++ dotfiles/.config/nvim/lua/plugins/main.lua
@@ -221,6 +221,7 @@ return {
 		config = function()
 			require('conform').setup({
 				formatters_by_ft = {
+					nix = { 'nixfmt' },
 					lua = { 'stylua' },
 					python = { 'isort', 'ruff' },
 					javascript = { 'prettier' },
